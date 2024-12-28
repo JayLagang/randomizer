05464d341a55dbae085c41f7c54f378335874ed2
Marvin Lagang <marvinjaylagang01@gmail.com>
2024-12-29 02:01:11 +0800

    Add react-hot-toast for notifications and update dependencies

--- src/components/RandomWebsitePicker.js
+++ src/components/RandomWebsitePicker.js
@@ -2,9 +2,11 @@
 
 import { useState } from 'react';
 import { sites } from '@/data/sites';
+import Image from 'next/image';
+import { Toaster, toast } from 'react-hot-toast';
+
 export default function RandomWebsitePicker() {
   const websites = sites;
-
   const [randomWebsites, setRandomWebsites] = useState([]);
 
   const pickRandomWebsites = () => {
@@ -14,57 +16,62 @@ export default function RandomWebsitePicker() {
 
   const copyToClipboard = () => {
     const textToCopy = randomWebsites.join('\n');
-    navigator.clipboard.writeText(textToCopy).then(() => {
-      alert('Copied to clipboard!');
-    }).catch(err => {
-      alert('Failed to copy: ', err);
-    });
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        toast.success('Copied to clipboard!');
+      })
+      .catch(err => {
+        toast.error('Failed to copy: ' + err.message);
+      });
   };
 
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
-        <div className='flex justify-center'>
-            <h1 className='text-2xl font-semibold'>Random Website Picker</h1>
-        </div>
-        
-      
-        
-        {randomWebsites.length > 0 && (
-            <div style={{ marginTop: "20px" }}>
-            <h2 className='font-bold my-5'>SELECTED WEBSITES</h2>
-            <ul>
-                {randomWebsites.map((website, index) => (
-                <li key={index}>
-                    <a href={website} target="_blank" rel="noopener noreferrer">
-                    {website}
-                    </a>
-                </li>
-                ))}
-            </ul>
-            
-            </div>
-        )}
+      {/* Toaster for notifications */}
+      <Toaster position=" bottom-right" />
 
-        <div className='mx-auto justify-center mt-3 space-y-2'>
-            <div>
-                <button 
-                onClick={pickRandomWebsites} 
-                className='border p-3 rounded-md border-white'
-                
-                >
-                Pick 20 Random Websites
-                </button>
-            </div>
-            <div>
-                <button 
-                    onClick={copyToClipboard}
-                    className=' border p-3 rounded-md border-white ' >
-                    Copy to Clipboard
-                </button>
-            </div>
-            
+      <Image src='/images/bg_image.jpg' className='rounded-md mx-auto mb-2' width={100} height={100} alt='adong' />
+      <div className='flex justify-center'>
+        <h1 className='text-2xl font-semibold'>Random Website Picker ni Adong</h1>
+      </div>
+      {randomWebsites.length === 0 && (
+        <div>
+          <p className='my-5'>Click the button below to pick 20 random websites from the list.</p>
         </div>
-        
+      )}
+      {randomWebsites.length > 0 && (
+        <div style={{ marginTop: "20px" }}>
+          <h2 className='font-bold my-5'>SELECTED WEBSITES</h2>
+          <ul>
+            {randomWebsites.map((website, index) => (
+              <li key={index}>
+                <a href={website} target="_blank" rel="noopener noreferrer">
+                  {website}
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+      )}
+
+      <div className='mx-auto justify-center mt-5 space-y-2'>
+        <div>
+          <button
+            onClick={pickRandomWebsites}
+            className='border p-3 rounded-md border-white'
+          >
+            Pick 20 Random Websites
+          </button>
+        </div>
+        <div>
+          <button
+            onClick={copyToClipboard}
+            className=' border p-3 rounded-md border-white '
+          >
+            Copy to Clipboard
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
